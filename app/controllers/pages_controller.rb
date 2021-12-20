3afe166d4e405804f9866d1b2b761e5ee552b8e4
Joao Bitencourt <joao.bitencourt@bairesdev.com>
2021-12-20 15:44:51 -0300

    Add pagination to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
   
   def home
-
-      query = "quantity > ?", 0
-      query = "quantity > ? and name LIKE ?", 0 ,"%#{params[:search][:search]}%" if params[:search]
-      @products = Vape.where(query) + Essence.where(query) + Accessory.where(query)
-      @products = @products.sort_by &:created_at
-
-      respond_to do |format|
-        format.html
-        format.json { render json: @products }
-      end
-
+    query = "quantity > ?", 0
+    query = "quantity > ? and name LIKE ?", 0 ,"%#{params[:search][:search]}%" if params[:search]
+    @products = Vape.where(query) + Essence.where(query) + Accessory.where(query)
+    @products = @products.sort_by{ |h| h[:updated_at] }.reverse
+    @products = @products.paginate(:page => params[:page], :per_page => 8) 
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => { :products => @products } }
+    end
   end
 end
