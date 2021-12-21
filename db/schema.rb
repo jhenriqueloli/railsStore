95972c1abea1f52966f88b6b1340f87a4539e800
Joao Bitencourt <joao.bitencourt@bairesdev.com>
2021-12-21 09:26:30 -0300

    fix homepage layout

--- db/schema.rb
+++ db/schema.rb
@@ -62,10 +62,10 @@ ActiveRecord::Schema.define(version: 2021_12_17_202708) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
